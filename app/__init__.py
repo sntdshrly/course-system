c7c817d48ba01b3e2b7ab21ba81c48bef2325881
Arya Majiah <71741042+aryatriputram@users.noreply.github.com>
2021-12-13 20:08:24 +0700

    buat untuk deploy

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = ""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:wasdfer0918@localhost/rpl'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://webpk_28783064:wasdfer0918@sql202.freeweb.pk/webpk_28783064_rpl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b2700f33b80883:e62dadf1@us-cdbr-east-05.cleardb.net/heroku_25a5a21d8989cd7?reconnect=true'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
