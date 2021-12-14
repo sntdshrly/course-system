d68c274cec448b36277e77020ab008f304577b28
Arya Majiah <71741042+aryatriputram@users.noreply.github.com>
2021-12-14 14:24:48 +0700

    ganti url db

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = ""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:wasdfer0918@localhost/rpl'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b2700f33b80883:e62dadf1@us-cdbr-east-05.cleardb.net/heroku_25a5a21d8989cd7'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:wasdfer0918@localhost/rpl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b2700f33b80883:e62dadf1@us-cdbr-east-05.cleardb.net/heroku_25a5a21d8989cd7'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
 db = SQLAlchemy(app)
